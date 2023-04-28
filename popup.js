0961c18941af080d4e379e1287f460a14cbe19f9
12core1 <12533753+visnkmr@users.noreply.github.com>
2023-04-28 19:08:52 +0530

    set foldername to default to date
    set foldername to default date with time to the second to enable session save

--- popup.js
+++ popup.js
@@ -18,8 +18,31 @@ let mode = "active"; // Possible values are "active" or "all"
 
 // Define a function to update the textarea value based on the mode
 function updateTextarea() {
-  // Clear the textarea value
-  input.value = "";
+  // Define a function that returns today's date and time as a string
+function getTodayDateTimeString() {
+  // Create a new Date object with the current date and time
+  let today = new Date();
+  // Get the year, month, day, hour, minute and second from the Date object
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1; // Months are zero-based
+  let day = today.getDate();
+  let hour = today.getHours();
+  let minute = today.getMinutes();
+  let second = today.getSeconds();
+  // Pad the month, day, hour, minute and second with leading zeros if needed
+  month = month < 10 ? "0" + month : month;
+  day = day < 10 ? "0" + day : day;
+  hour = hour < 10 ? "0" + hour : hour;
+  minute = minute < 10 ? "0" + minute : minute;
+  second = second < 10 ? "0" + second : second;
+  // Return the date and time as a string in the format yyyy-mm-dd_hh-mm-ss
+  return year + "-" + month + "-" + day + "_" + hour + "-" + minute + "-" + second;
+}
+
+// Call the function and print the result
+console.log(getTodayDateTimeString());
+
+folder.innerHTML=getTodayDateTimeString();
 
   // Check the mode
   if (mode === "active") {
