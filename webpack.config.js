e290df7658a485c2326643986817b63a63ea5c14
12core1 <12533753+visnkmr@users.noreply.github.com>
2023-11-30 01:24:32 +0530

    works to save to db

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,14 @@ module.exports = {
     rules: [
       {
         test: /\.ts$/, // match TypeScript files
-        use: 'ts-loader', // use ts-loader to transpile them
+        use: {
+          loader:'ts-loader',
+          options: {
+            transpileOnly: true
+          } // use ts-loader to transpile them
+        },
         exclude: /node_modules/, // exclude node_modules directory
+        
       },
     ],
   },
